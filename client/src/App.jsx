30c8a2a488f1986084866a9c0b1aa8cf50c990e4
FaRusDev <fadlirusandy@gmail.com>
2022-11-17 00:05:27 +0800

    initial commit with some code changes

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+
+
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
+
+const App = () => {
+  return (
+    <Container maxWidth="lg">
+      <AppBar postion="static" color="inherit">
+        <Typography variant="h2" align='center'>Memories</Typography>
+        <img src={memories} alt="memories" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
+}
+
+export default App
